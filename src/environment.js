e2957f8247c5a858f8fe6de7cf22a3551d627164
Nikhil991127 <142004448+Nikhil991127@users.noreply.github.com>
2026-04-30 11:18:38 +0530

    Fix server URL for production environment

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://meetsphere-back.onrender.com" :
+    "https://meetsphere-back-r9np.onrender.com" :
     "http://localhost:8000"
 
 export default server;
